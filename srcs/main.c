f5e2c85d6ad6e94f56e09be548ed5bd8a45b12d9
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-02-14 18:33:38 +0100

    Remove template example files

--- srcs/main.c
+++ srcs/main.c
@@ -6,27 +6,15 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/14 15:15:12 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/14 15:25:12 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/14 18:29:06 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "hello.h"
 #include "ft_program.h"
+#include "ft_streams.h"
 
-int		main(int ac, char **av)
+int		main(void)
 {
-	int		i;
-
-	if (ac > 1)
-	{
-		i = 1;
-		while (i < ac)
-		{
-			hello(av[i]);
-			++i;
-		}
-	}
-	else
-		hello("you");
+	ft_fputs("Hello, world!\n", FT_STDOUT);
 	return (ft_cleanup(FT_EXIT_SUCCESS));
 }
