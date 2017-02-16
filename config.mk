b9ace9b391bfabd473799bd3ef5f9d66b36b864c
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-02-17 00:00:53 +0100

    Add function to unset an environment variable

--- config.mk
+++ config.mk
@@ -6,7 +6,7 @@
 #    By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2017/02/14 14:04:23 by jguyon            #+#    #+#              #
-#    Updated: 2017/02/16 23:07:31 by jguyon           ###   ########.fr        #
+#    Updated: 2017/02/16 23:52:44 by jguyon           ###   ########.fr        #
 #                                                                              #
 #******************************************************************************#
 
@@ -62,6 +62,7 @@ SOURCES = \
 	ms_env_end \
 	ms_env_get \
 	ms_env_set \
+	ms_env_unset \
 
 # Wildcard for test files, it makes experimentation easier
 TEST_FILES := $(basename $(notdir $(wildcard $(TEST_PATH)/*.c)))
--- tests/test_env.c
+++ tests/test_env.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/16 21:36:14 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/16 23:43:22 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/16 23:58:49 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,9 +54,27 @@ static void	test_env_set(t_tap *t)
 		FT_TAP_SEQ(t, env.envp[1], "HOST=laptop");
 		FT_TAP_SEQ(t, env.envp[0], "USER=john");
 		FT_TAP_UEQ(t, (ptrdiff_t)env.envp[2], (ptrdiff_t)NULL);
+		FT_TAP_UEQ(t, env.size, 2);
 		FT_TAP_IEQ(t, ms_env_set(&env, "HOME", "/home/john"), 0);
 		FT_TAP_SEQ(t, env.envp[2], "HOME=/home/john");
 		FT_TAP_UEQ(t, (ptrdiff_t)env.envp[3], (ptrdiff_t)NULL);
+		FT_TAP_UEQ(t, env.size, 3);
+		ms_env_end(&env);
+	}
+}
+
+static void	test_env_unset(t_tap *t)
+{
+	char	*envp[] = {"USER=john", "HOST=desktop", "HOME=/home/john", NULL};
+	t_env	env;
+
+	if (FT_TAP_IEQ(t, ms_env_start(&env, envp), 0))
+	{
+		ms_env_unset(&env, "HOST");
+		FT_TAP_SEQ(t, env.envp[0], "USER=john");
+		FT_TAP_SEQ(t, env.envp[1], "HOME=/home/john");
+		FT_TAP_UEQ(t, (ptrdiff_t)env.envp[2], (ptrdiff_t)NULL);
+		FT_TAP_UEQ(t, env.size, 2);
 		ms_env_end(&env);
 	}
 }
@@ -66,4 +84,5 @@ void		run_tests(t_tap *t)
 	FT_TAP_TEST(t, test_env_init);
 	FT_TAP_TEST(t, test_env_get);
 	FT_TAP_TEST(t, test_env_set);
+	FT_TAP_TEST(t, test_env_unset);
 }
