fbc79bc49acc569afe8ec427ee26f9c268c4dd08
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-02-17 20:48:57 +0100

    Add way to search for binary in env PATH

--- srcs/ms_env_end.c
+++ srcs/ms_env_end.c
@@ -6,14 +6,23 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/16 21:52:55 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/16 21:57:11 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/17 20:08:03 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ms_env.h"
 #include "ft_memory.h"
 
-void	ms_env_end(t_env *env)
+static int	destroy_bin(void *entry, void *acc)
+{
+	(void)acc;
+	ft_dlst_remove(&((t_bin *)entry)->node);
+	ft_memdel((void **)&(((t_bin *)entry)->file));
+	ft_memdel(&entry);
+	return (1);
+}
+
+void		ms_env_end(t_env *env)
 {
 	size_t	i;
 
@@ -23,5 +32,6 @@ void	ms_env_end(t_env *env)
 		ft_memdel((void **)&(env->envp[i]));
 		++i;
 	}
+	ft_dlst_foreachl(&(env->bins), NULL, &destroy_bin);
 	ft_memdel((void **)&(env->envp));
 }
