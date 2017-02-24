2a512a2a13b1dd87e6b0b7d952edeeb066ccd5a3
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-02-24 23:54:22 +0100

    Document the new sh_parse module

--- includes/sh_ast.h
+++ includes/sh_ast.h
@@ -6,25 +6,42 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 18:22:27 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/24 22:59:59 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/24 23:49:45 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SH_AST_H
 # define SH_AST_H
 
+/*
+** Utilities to create and manipulate an AST of the shell language
+*/
+
 # include "ft_dlists.h"
 
+/*
+** Type representing a word (can be a command name, an arg...)
+*/
 typedef struct	s_sh_word {
 	char			*str;
 	t_dlist_node	node;
 }				t_sh_word;
 
+/*
+** Type representing a simple command
+*/
 typedef struct	s_sh_cmd {
 	t_sh_word	*name;
 	t_dlist		args;
 }				t_sh_cmd;
 
+/*
+** Functions to manipulate the AST
+**
+** The sh_word_tostr and sh_cmd_toargv extract their return value
+** from the argument before freeing it.
+** The other functions are somewhat self-explanatory, I think.
+*/
 t_sh_word		*sh_word_new(char *str);
 char			*sh_word_tostr(t_sh_word **word);
 void			sh_word_del(t_sh_word **word);
--- includes/sh_parse.h
+++ includes/sh_parse.h
@@ -6,12 +6,16 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/24 15:59:10 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/24 22:12:05 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/24 23:53:43 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SH_PARSE_H
 # define SH_PARSE_H
+
+/*
+** Functions used to parse the input of the program
+*/
 
 # include "sh_ast.h"
 # include "ft_streams.h"
@@ -19,13 +23,35 @@
 # define SH_NEWLINE		'\n'
 # define SH_WHITESPACE	" \t"
 
+/*
+** Type representing the program input
+*/
 typedef struct	s_sh_input {
 	int			next_c;
 	t_stream	*stm;
 }				t_sh_input;
 
+/*
+** Init the input from a stream
+**
+** Returns 0 if successful, or an error number.
+*/
 int				sh_init_input(t_sh_input *in, t_stream *stm);
+
+/*
+** Parse a word from the input
+**
+** Returns 0 if successful, -1 if the input has ended before a word
+** could be read, an error number if an error occured.
+*/
 int				sh_parse_word(t_sh_input *in, t_sh_word **word);
+
+/*
+** Parse a command with arguments from the input
+**
+** Returns 0 if successful, -1 if the input has ended before a word
+** could be read, an error number if an error occured.
+*/
 int				sh_parse_cmd(t_sh_input *in, t_sh_cmd **cmd);
 
 #endif
