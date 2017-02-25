e41f51d4aba2b98fe7393482d146f27e70750a05
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-02-25 23:50:36 +0100

    Make exit print an error when arg is not number

--- srcs/sh_builtin_exit.c
+++ srcs/sh_builtin_exit.c
@@ -6,16 +6,35 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/25 03:04:10 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/25 23:24:34 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/25 23:49:59 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_builtins.h"
 #include "sh_errors.h"
 #include "ft_program.h"
-#include "ft_numbers.h"
+#include "ft_chars.h"
 
-int		sh_builtin_exit(int ac, char *const av[], t_sh_env *env)
+static int	strtoi(const char *str)
+{
+	unsigned int	n;
+
+	if (*str == '\0')
+		return (-1);
+	n = 0;
+	while (ft_isdigit(*str))
+	{
+		n = 10 * n + (*str - '0');
+		if (n > 255)
+			return (-1);
+		++str;
+	}
+	if (*str != '\0')
+		return (-1);
+	return (n);
+}
+
+int			sh_builtin_exit(int ac, char *const av[], t_sh_env *env)
 {
 	int		status;
 
@@ -24,7 +43,14 @@ int		sh_builtin_exit(int ac, char *const av[], t_sh_env *env)
 		ft_error(0, SH_ERR_ARGS2BIG, "%s", av[0]);
 		return (FT_EXIT_FAILURE);
 	}
-	status = ac > 1 ? ft_atoi(av[1]) : sh_env_status(env);
+	else if (ac < 2)
+		status = sh_env_status(env);
+	else if ((status = strtoi(av[1])) < 0)
+	{
+		ft_error(0, SH_ERR_BADNUM, "%s: %s", av[0], av[1]);
+		sh_env_exit(env, FT_EXIT_FAILURE);
+		return (FT_EXIT_FAILURE);
+	}
 	sh_env_exit(env, status);
 	return (status);
 }
