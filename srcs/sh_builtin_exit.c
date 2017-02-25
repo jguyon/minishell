75e7c98c849da04354336f5400f5b814c2042825
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-02-25 23:26:09 +0100

    Make exit built-in fail if too many args

--- srcs/sh_builtin_exit.c
+++ srcs/sh_builtin_exit.c
@@ -6,17 +6,24 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/25 03:04:10 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/25 03:13:41 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/25 23:24:34 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sh_builtins.h"
+#include "sh_errors.h"
+#include "ft_program.h"
 #include "ft_numbers.h"
 
 int		sh_builtin_exit(int ac, char *const av[], t_sh_env *env)
 {
 	int		status;
 
+	if (ac > 2)
+	{
+		ft_error(0, SH_ERR_ARGS2BIG, "%s", av[0]);
+		return (FT_EXIT_FAILURE);
+	}
 	status = ac > 1 ? ft_atoi(av[1]) : sh_env_status(env);
 	sh_env_exit(env, status);
 	return (status);
--- tests/test_builtin_exit.c
+++ tests/test_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/25 03:14:57 by jguyon            #+#    #+#             */
-/*   Updated: 2017/02/25 03:20:59 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/02/25 23:25:05 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,23 @@ static void	test_no_arg(t_tap *t)
 		env.exit_status = 3;
 		FT_TAP_IEQ(t, sh_builtin_exit(1, av, &env), 3);
 		FT_TAP_IEQ(t, env.exit_status, 3);
+		sh_env_end(&env);
+	}
+}
+
+static void	test_few_args(t_tap *t)
+{
+	char		*ep[] = {NULL};
+	t_sh_env	env;
+	char		*av[] = {"exit", "42", "24", NULL};
+
+	ft_tap_plan(t, 3);
+	if (sh_env_start(&env, ep) == 0)
+	{
+		env.exit_status = 3;
+		FT_TAP_IEQ(t, sh_builtin_exit(3, av, &env), 1);
+		FT_TAP_IEQ(t, env.exit_status, 3);
+		STDERR_EQ(t, "minishell: exit: too many arguments\n");
 		sh_env_end(&env);
 	}
 }
@@ -48,5 +65,6 @@ static void	test_num_arg(t_tap *t)
 void		run_tests(t_tap *t)
 {
 	FT_TAP_TEST(t, test_no_arg);
+	FT_TAP_TEST(t, test_few_args);
 	FT_TAP_TEST(t, test_num_arg);
 }
