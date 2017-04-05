c396a575b0dc5e8cf89639da5060ee346b7f990b
Jérôme Guyon <jeromep.guyon@gmail.com>
2017-04-05 16:37:28 +0200

    Add support for escaping with backslash

--- tests/test_lexer_token.c
+++ tests/test_lexer_token.c
@@ -6,7 +6,7 @@
 /*   By: jguyon <jguyon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/05 13:27:14 by jguyon            #+#    #+#             */
-/*   Updated: 2017/04/05 14:35:52 by jguyon           ###   ########.fr       */
+/*   Updated: 2017/04/05 16:34:25 by jguyon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,6 +115,46 @@ static void	test_newline_after_word(t_tap *t)
 	ft_fclose(stm);
 }
 
+static void	test_escape_whitespace(t_tap *t)
+{
+	char		in[] = "hello\\ world";
+	t_stream	*stm;
+	t_sh_lexer	lex;
+	t_sh_token	tok;
+
+	stm = ft_fmemopen(in, sizeof(in) - 1, "r");
+	FT_TAP_IEQ(t, sh_lexer_init(&lex, stm), 0);
+	FT_TAP_IEQ(t, sh_lexer_token(&lex, &tok), 0);
+	FT_TAP_IEQ(t, tok.type, SH_TOKEN_WORD);
+	FT_TAP_SEQ(t, tok.data.word, "hello world");
+	free(tok.data.word);
+	FT_TAP_IEQ(t, sh_lexer_token(&lex, &tok), 0);
+	FT_TAP_IEQ(t, tok.type, SH_TOKEN_EOI);
+	ft_fclose(stm);
+}
+
+static void	test_line_continuation(t_tap *t)
+{
+	char		in[] = "hello \\\n world";
+	t_stream	*stm;
+	t_sh_lexer	lex;
+	t_sh_token	tok;
+
+	stm = ft_fmemopen(in, sizeof(in) - 1, "r");
+	FT_TAP_IEQ(t, sh_lexer_init(&lex, stm), 0);
+	FT_TAP_IEQ(t, sh_lexer_token(&lex, &tok), 0);
+	FT_TAP_IEQ(t, tok.type, SH_TOKEN_WORD);
+	FT_TAP_SEQ(t, tok.data.word, "hello");
+	free(tok.data.word);
+	FT_TAP_IEQ(t, sh_lexer_token(&lex, &tok), 0);
+	FT_TAP_IEQ(t, tok.type, SH_TOKEN_WORD);
+	FT_TAP_SEQ(t, tok.data.word, "world");
+	free(tok.data.word);
+	FT_TAP_IEQ(t, sh_lexer_token(&lex, &tok), 0);
+	FT_TAP_IEQ(t, tok.type, SH_TOKEN_EOI);
+	ft_fclose(stm);
+}
+
 void		run_tests(t_tap *t)
 {
 	FT_TAP_TEST(t, test_empty);
@@ -123,4 +163,6 @@ void		run_tests(t_tap *t)
 	FT_TAP_TEST(t, test_multiple_words);
 	FT_TAP_TEST(t, test_newline_only);
 	FT_TAP_TEST(t, test_newline_after_word);
+	FT_TAP_TEST(t, test_escape_whitespace);
+	FT_TAP_TEST(t, test_line_continuation);
 }
